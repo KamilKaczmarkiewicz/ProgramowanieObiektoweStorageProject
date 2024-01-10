1c6706797559811fec0366c3742b6d999e79d15a
KamilKaczmarkiewicz <kaczmarkiewicz98@gmail.com>
2024-01-10 14:56:32 +0100

    created CRUD for item with pages

--- ProgramowanieObiektoweStorageProject/Pages/Index.cshtml.cs
+++ ProgramowanieObiektoweStorageProject/Pages/Index.cshtml.cs
@@ -1,20 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProgramowanieObiektoweStorageProject.DbServices;
+using ProgramowanieObiektoweStorageProject.Models;
 
 namespace ProgramowanieObiektoweStorageProject.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly AppDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(AppDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<Item> Item { get; set; } = default!;
 
+        public async Task OnGetAsync()
+        {
+            Item = await _context.Items.ToListAsync();
         }
     }
 }
